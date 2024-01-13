dc34407f7af2bde391cf0e9ba3681a5154a6a0aa
aimaaan <aimnfauzi98@gmail.com>
2024-01-14 02:07:32 +0800

    clean admin
    clean sidebar

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -67,68 +67,7 @@
           </div>
           <!-- Tasks end-->
           <!-- Megamenu-->
-          <div class="list-inline-item dropdown menu-large"><a href="#" data-toggle="dropdown" class="nav-link">Mega <i class="fa fa-ellipsis-v"></i></a>
-            <div class="dropdown-menu megamenu">
-              <div class="row">
-                <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading</strong>
-                  <ul class="list-unstyled mb-3">
-                    <li><a href="#">Lorem ipsum dolor</a></li>
-                    <li><a href="#">Sed ut perspiciatis</a></li>
-                    <li><a href="#">Voluptatum deleniti</a></li>
-                    <li><a href="#">At vero eos</a></li>
-                    <li><a href="#">Consectetur adipiscing</a></li>
-                    <li><a href="#">Duis aute irure</a></li>
-                    <li><a href="#">Necessitatibus saepe</a></li>
-                    <li><a href="#">Maiores alias</a></li>
-                  </ul>
-                </div>
-                <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading</strong>
-                  <ul class="list-unstyled mb-3">
-                    <li><a href="#">Lorem ipsum dolor</a></li>
-                    <li><a href="#">Sed ut perspiciatis</a></li>
-                    <li><a href="#">Voluptatum deleniti</a></li>
-                    <li><a href="#">At vero eos</a></li>
-                    <li><a href="#">Consectetur adipiscing</a></li>
-                    <li><a href="#">Duis aute irure</a></li>
-                    <li><a href="#">Necessitatibus saepe</a></li>
-                    <li><a href="#">Maiores alias</a></li>
-                  </ul>
-                </div>
-                <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading</strong>
-                  <ul class="list-unstyled mb-3">
-                    <li><a href="#">Lorem ipsum dolor</a></li>
-                    <li><a href="#">Sed ut perspiciatis</a></li>
-                    <li><a href="#">Voluptatum deleniti</a></li>
-                    <li><a href="#">At vero eos</a></li>
-                    <li><a href="#">Consectetur adipiscing</a></li>
-                    <li><a href="#">Duis aute irure</a></li>
-                    <li><a href="#">Necessitatibus saepe</a></li>
-                    <li><a href="#">Maiores alias</a></li>
-                  </ul>
-                </div>
-                <div class="col-lg-3 col-md-6"><strong class="text-uppercase">Elements Heading</strong>
-                  <ul class="list-unstyled mb-3">
-                    <li><a href="#">Lorem ipsum dolor</a></li>
-                    <li><a href="#">Sed ut perspiciatis</a></li>
-                    <li><a href="#">Voluptatum deleniti</a></li>
-                    <li><a href="#">At vero eos</a></li>
-                    <li><a href="#">Consectetur adipiscing</a></li>
-                    <li><a href="#">Duis aute irure</a></li>
-                    <li><a href="#">Necessitatibus saepe</a></li>
-                    <li><a href="#">Maiores alias</a></li>
-                  </ul>
-                </div>
-              </div>
-              <div class="row megamenu-buttons text-center">
-                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-1"><i class="fa fa-clock-o"></i><strong>Demo 1</strong></a></div>
-                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-2"><i class="fa fa-clock-o"></i><strong>Demo 2</strong></a></div>
-                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-3"><i class="fa fa-clock-o"></i><strong>Demo 3</strong></a></div>
-                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link dashbg-4"><i class="fa fa-clock-o"></i><strong>Demo 4</strong></a></div>
-                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link bg-danger"><i class="fa fa-clock-o"></i><strong>Demo 5</strong></a></div>
-                <div class="col-lg-2 col-md-4"><a href="#" class="d-block megamenu-button-link bg-info"><i class="fa fa-clock-o"></i><strong>Demo 6</strong></a></div>
-              </div>
-            </div>
-          </div>
+          
           <!-- Megamenu end     -->
           <!-- Languages dropdown    -->
           <!-- Log out               -->
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -23,18 +23,6 @@
                 <a href="forms.html"> <i class="icon-padnote"></i>Forms</a>
             </li>
 
-            <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-windows"></i>Example dropdown </a>
-              <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
-                <li><a href="#">Page</a></li>
-                <li><a href="#">Page</a></li>
-                <li><a href="#">Page</a></li>
-              </ul>
-            </li>
             <li><a href="login.html"> <i class="icon-logout"></i>Login page </a></li>
-    </ul><span class="heading">Extras</span>
-    <ul class="list-unstyled">
-      <li> <a href="#"> <i class="icon-settings"></i>Demo </a></li>
-      <li> <a href="#"> <i class="icon-writing-whiteboard"></i>Demo </a></li>
-      <li> <a href="#"> <i class="icon-chart"></i>Demo </a></li>
-    </ul>
+  
   </nav>
