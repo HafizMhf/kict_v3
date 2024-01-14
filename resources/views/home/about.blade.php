a97515db0458f185dc97a14c141e99d47125e8c0
aimaaan <aimnfauzi98@gmail.com>
2024-01-14 14:03:28 +0800

    update about

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -9,7 +9,7 @@
              </div>
           </div>
           <div class="col-md-6 padding_right_0">
-             <div><img src="images/about-img.png" class="about_img"></div>
+             <div><img src="images/kict.jpeg" class="about_img"></div>
           </div>
        </div>
     </div>
