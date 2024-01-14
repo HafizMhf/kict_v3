0823b244944487ef5df08c490595012e905990de
aimaaan <aimnfauzi98@gmail.com>
2024-01-14 09:53:14 +0800

    update footer n sidebar n index.html

--- resources/views/admin/footer.blade.php
+++ resources/views/admin/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="footer__block block no-margin-bottom">
       <div class="container-fluid text-center">
         <!-- Please do not remove the backlink to us unless you support us at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
-         <p class="no-margin-bottom">2018 &copy; Your company. Download From <a target="_blank" href="https://templateshub.net">Templates Hub</a>.</p>
+         <p class="no-margin-bottom">2023 &copy; F&S  <a target="_blank" href="https://templateshub.net">Web4Life</a>.</p>
       </div>
     </div>
   </footer>
