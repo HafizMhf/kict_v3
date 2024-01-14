3fd18856df7aec3ad79c169efbcef563b5548d3c
puterifarhana <92302315+puterifarhana@users.noreply.github.com>
2024-01-14 10:08:52 +0800

    update banner

--- resources/views/home/banner.blade.php
+++ resources/views/home/banner.blade.php
@@ -3,23 +3,23 @@
        <div class="carousel-inner">
           <div class="carousel-item active">
              <div class="container">
-                <h1 class="banner_taital">Adventure</h1>
-                <p class="banner_text">There are many variations of passages of Lorem Ipsum available, but the majority have sufferedThere are ma available, but the majority have suffered</p>
-                <div class="read_bt"><a href="#">Get A Quote</a></div>
+                <h1 class="banner_taital">Feedback</h1>
+                <p class="banner_text">Share Your Thoughts</p>
+                <div class="read_bt"><a href="#">Submit Feedback</a></div>
              </div>
           </div>
           <div class="carousel-item">
              <div class="container">
-                <h1 class="banner_taital">Adventure</h1>
-                <p class="banner_text">There are many variations of passages of Lorem Ipsum available, but the majority have sufferedThere are ma available, but the majority have suffered</p>
-                <div class="read_bt"><a href="#">Get A Quote</a></div>
+                <h1 class="banner_taital">Suggestion</h1>
+                <p class="banner_text">Share Your Ideas</p>
+                <div class="read_bt"><a href="#">Submit Suggestion</a></div>
              </div>
           </div>
           <div class="carousel-item">
              <div class="container">
-                <h1 class="banner_taital">Adventure</h1>
-                <p class="banner_text">There are many variations of passages of Lorem Ipsum available, but the majority have sufferedThere are ma available, but the majority have suffered</p>
-                <div class="read_bt"><a href="#">Get A Quote</a></div>
+                <h1 class="banner_taital">Improvement</h1>
+                <p class="banner_text">Help Us Improve</p>
+                <div class="read_bt"><a href="#">Submit Improvement</a></div>
              </div>
           </div>
        </div>
